8c2c7887eef504daa2cd952b51cd5c149bcaf2ba
Braeden42 <Braeden42.braedenfillmore42@gmail.com>
2019-02-04 08:47:04 -0800

    AA

---
+++ icebowlgit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  Braeden42;
+git config --global user.name  Braeden42;
+git config --global user.user Braeden42;
+git config --global user.email braedenfillmore42@gmail.com;
+git config --list;
